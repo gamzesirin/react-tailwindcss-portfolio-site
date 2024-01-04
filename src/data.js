573d73736084e15431a0f8413ae6ea1de83242b5
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-04 20:28:27 +0300

    project added.

---
+++ src/data.js
@@ -0,0 +1,95 @@
+import { CSS3, Express, HTML5, Javascript, Mongodb, Nextjs, Nextjs2, Nodejs, Python, Reactjs } from './assets'
+
+export const projectStats = [
+	{
+		name: 'Total Projects',
+		value: '250+'
+	},
+	{
+		name: 'Website Design',
+		value: '100+'
+	},
+	{
+		name: 'Mobile App Design',
+		value: '60'
+	}
+]
+
+export const tools = [
+	{
+		name: 'MongoDB',
+		icon: Mongodb
+	},
+	{
+		name: 'Express',
+		icon: Express
+	},
+	{
+		name: 'React',
+		icon: Reactjs
+	},
+	{
+		name: 'NodeJs',
+		icon: Nodejs
+	},
+	{
+		name: 'NextJs',
+		icon: Nextjs2
+	}
+]
+
+export const skills = [
+	{
+		name: 'ReactJs',
+		value: 75,
+		icon: Reactjs
+	},
+	{
+		name: 'NodeJs',
+		value: 96,
+		icon: Nodejs
+	},
+	{
+		name: 'NextJs',
+		value: 85,
+		icon: Nextjs
+	},
+	{
+		name: 'MongoDB',
+		value: 97,
+		icon: Mongodb
+	},
+	{
+		name: 'CSS3',
+		value: 99,
+		icon: CSS3
+	},
+	{
+		name: 'HTML5',
+		value: 99,
+		icon: HTML5
+	},
+	{
+		name: 'JavaScript',
+		value: 88,
+		icon: Javascript
+	},
+	{
+		name: 'Python',
+		value: 98,
+		icon: Python
+	}
+]
+
+export const softskills = [
+	'Time management',
+	'Communication',
+	'Empathy',
+	'Teamwork',
+	'Helpfulness',
+	'Creativity',
+	'Self-learning',
+	'Accountability',
+	'Problem solving',
+	'Open-mindedness'
+]
