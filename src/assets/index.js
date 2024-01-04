a9d696d4ed7dec96ec0e3050f33858654a6c65e4
Gamze Şirin <gamze1sirin@gmail.com>
2024-01-04 20:29:55 +0300

    added.

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+export { default as Mongodb } from './mongodb.svg'
+export { default as Reactjs } from './reactjs.svg'
+export { default as Nodejs } from './nodejs.svg'
+
+export { default as Redux } from './redux.svg'
+export { default as Express } from './express.svg'
+export { default as Nextjs } from './nextjs.svg'
+export { default as Nextjs2 } from './nextjs2.svg'
+
+export { default as CSS3 } from './css3.svg'
+export { default as HTML5 } from './html.svg'
+export { default as Javascript } from './javascript.svg'
+export { default as Python } from './python.svg'
+
+export { default as Profile } from './profile.png'
+export { default as Profile2 } from './profile2.png'
